e057b313ab70ed151ac5e25a123e69cdc59ba96e
Nicholas Maspoch <nmaspoch@hotmail.com>
2024-07-14 17:08:54 -0400

    Site loads default profile

--- app.js
+++ app.js
@@ -33,11 +33,32 @@ app.set("views, views");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
+// app.use((req, res, next) => {
+//   Profile.findByPk(1)
+//     .then((profile) => (req.profile = profile))
+//     .catch((err) => console.log(err));
+// });
+
 app.use(fitnessRoutes);
 
 app.use(errorController.get404);
 
 sequelize
   .sync()
+  .then((result) => {
+    return Profile.findByPk(1);
+  })
+  .then((profile) => {
+    if (!profile) {
+      return Profile.create({
+        name: "Nicholas Maspoch",
+        weight: 130,
+        age: 20,
+        height: 158,
+        goals: "Lose fat",
+      });
+    }
+    return profile;
+  })
   .then((result) => app.listen(3000))
   .catch((err) => console.log(err));
